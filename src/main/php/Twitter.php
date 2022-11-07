2170fd9c67873498ae6b9635c49551f3eaf1abab
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2022-11-07 17:14:48 +0100

    Add Todo after review

--- src/main/php/Twitter.php
+++ src/main/php/Twitter.php
@@ -16,6 +16,8 @@ class Twitter implements IObservable
     
     public function __construct(array $observers = [], array $twits = null)
     {
+        //TODO Review - Single Responsibility Principle
+        //Prefer to use the specialized method designed to add followers...
         $this->observers = $observers;
         $this->twits = $twits;
     }
@@ -44,6 +46,7 @@ class Twitter implements IObservable
     public function getObservers(): array
     {
         if ($this->observers == null) {
+            //TODO Review - There is a another way to avoid "null pointer exception"
             $this->observers = [];
         }
         return $this->observers;
@@ -52,12 +55,14 @@ class Twitter implements IObservable
     public function setObservers($observers): array
     {
         if ($this->observers == null) {
+            //TODO Review - Bad idea....
             $this->observers = $observers;
         } else {
             foreach ($observers as $observer) {
                 if (in_array($observer, $this->observers, true)) {
                     throw new SubscriberAlreadyExistsException();
                 } else {
+                    //TODO Review - If the exception is thrown... does it make any sens to write an else ?
                     $this->observers[] = $observer;
                 }
             }
@@ -68,6 +73,7 @@ class Twitter implements IObservable
     
     public function notifyObservers(): void
     {
+        //TODO REVIEW please clean your code (no commented code anymore)
         //throw new EmptyListOfSubscribersException();
         throw new EmptyListOfSubscribersException();
     }
@@ -75,6 +81,7 @@ class Twitter implements IObservable
     public function getTwits(): array
     {
         if ($this->twits == null) {
+            //TODO Review - There is a another way to avoid "null pointer exception"
             $this->twits = [];
         }
         return $this->twits;
