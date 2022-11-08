4feba7c87f2511339d5445f005acbca9e4c98e79
Luís Pedro Pinheiro <lpodevco@gmail.com>
2022-11-08 22:41:51 +0100

    Update Twitter.php

--- src/main/php/Twitter.php
+++ src/main/php/Twitter.php
@@ -9,84 +9,74 @@ require 'IObservable.php';
 class Twitter implements IObservable
 {
     //region private attributes
-    private array $observers;
-    private $twits;
-    
+    private array $observers = [];
+    private array $twits = [];
+
     //endregion private attributes
-    
-    public function __construct(array $observers = [], array $twits = null)
+
+    public function __construct(array $observers = [], array $twits = [])
     {
-        //TODO Review - Single Responsibility Principle
-        //Prefer to use the specialized method designed to add followers...
-        $this->observers = $observers;
-        $this->twits = $twits;
+        $this->setObservers($observers);
+        $this->setTwits($twits);
     }
-    
+
     public function subscribe(array $observers): void
     {
         $this->setObservers($observers);
-        
+
     }
-    
+
     public function unsubscribe(IObserver $observer): void
     {
-        if ($this->getObservers() == null) {
+        if (empty($this->getObservers())) {
             throw new EmptyListOfSubscribersException;
-        } else {
-            $key = array_search($observer, $this->getObservers(), true);
-            if ($key === false) {
-                throw new SubscriberNotFoundException;
-                
-            } else {
-                unset($this->observers[$key]);
-            }
         }
+
+        $key = array_search($observer, $this->getObservers(), true);
+
+        if ($key === false) {
+            throw new SubscriberNotFoundException;
+        }
+
+        unset($this->observers[$key]);
     }
-    
+
     public function getObservers(): array
     {
-        if ($this->observers == null) {
-            //TODO Review - There is a another way to avoid "null pointer exception"
+        if (empty($this->observers)) {
             $this->observers = [];
         }
         return $this->observers;
     }
-    
+
     public function setObservers($observers): array
     {
-        if ($this->observers == null) {
-            //TODO Review - Bad idea....
+        if (empty($this->observers)) {
             $this->observers = $observers;
         } else {
             foreach ($observers as $observer) {
                 if (in_array($observer, $this->observers, true)) {
                     throw new SubscriberAlreadyExistsException();
-                } else {
-                    //TODO Review - If the exception is thrown... does it make any sens to write an else ?
-                    $this->observers[] = $observer;
                 }
+                $this->observers[] = $observer;
             }
         }
-        
         return $this->observers;
     }
-    
+
     public function notifyObservers(): void
     {
-        //TODO REVIEW please clean your code (no commented code anymore)
-        //throw new EmptyListOfSubscribersException();
         throw new EmptyListOfSubscribersException();
     }
-    
+
     public function getTwits(): array
     {
-        if ($this->twits == null) {
-            //TODO Review - There is a another way to avoid "null pointer exception"
+        if (empty($this->twits)) {
             $this->twits = [];
         }
         return $this->twits;
     }
-    
+
     public function setTwits($twits): array
     {
         $this->twits = $twits;
